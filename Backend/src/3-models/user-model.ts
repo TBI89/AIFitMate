3efb73a6056cab9ebf996d132f95ecf306c5db64
TBI89 <tomerben89@gmail.com>
2024-02-18 20:25:28 +0200

    Improved password validation

--- Backend/src/3-models/user-model.ts
+++ Backend/src/3-models/user-model.ts
@@ -1,10 +1,12 @@
-import { Document, Schema, model } from "mongoose";
+import { Document, Schema, Types, model } from "mongoose";
+import { IRoleModel } from "./role-model";
 
 export interface IUserModel extends Document {
     firstName: string;
     lastName: string;
     email: string;
     password: string;
+    // role: IRoleModel | Types.ObjectId;
 }
 
 export const UserSchema = new Schema<IUserModel>({
@@ -14,26 +16,37 @@ export const UserSchema = new Schema<IUserModel>({
         required: [true, "Please enter your first name."],
         minlength: [2, "First name should contain at least 2 characters."],
         maxlength: [20, "First name can't contain more than 20 characters."],
+        trim: true, // Remove leading and trailing whitespaces
+        match: [/^[A-Za-z]+$/, "Only letters are allowed in the first name."],
     },
     lastName: {
         type: String,
         required: [true, "Please enter your last name."],
         minlength: [2, "Last name should contain at least 2 characters."],
         maxlength: [20, "Last name can't contain more than 20 characters."],
+        trim: true, // Remove leading and trailing whitespaces
+        match: [/^[A-Za-z]+$/, "Only letters are allowed in the last name."],
     },
     email: {
         type: String,
         required: [true, "Please enter your email address."],
+        unique: true, // Ensure email is unique
+        lowercase: true, // Convert email to lowercase
+        match: [
+            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+            "Please enter a valid email address."
+        ],
     },
     password: {
-        type: String,
-        required: [true, "Please enter your password."],
-        minlength: [8, "Password should contain at least 8 characters."],
-    }
+        type: String
+    },
+    // role: {
+    //     type: Types.ObjectId
+    // }
 },
     {
-        versionKey: false 
+        versionKey: false // Don't add the versionKey value of the document structure.
     }
 );
 
-export const UserModel = model<IUserModel>("UserModel", UserSchema, "users"); 
+export const UserModel = model<IUserModel>("UserModel", UserSchema, "users"); // "users" = collection name
--- Backend/src/5-services/auth-service.ts
+++ Backend/src/5-services/auth-service.ts
@@ -5,8 +5,14 @@ import { IUserModel, UserModel } from "../3-models/user-model";
 // Create new user on the db & generate JWT:
 async function register(user: IUserModel): Promise<string> {
     try {
+        console.log(user.password);
+        
+        if (!/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/.test(user.password)) {
+            throw new ValidationError('Password should contain at least 1 uppercase letter and 1 special character.');
+        }
         user.password = cyber.hashedPassword(user.password);
         const registeredUser = await UserModel.create(user);
+        console.log(user);
         const token = cyber.getNewToken(registeredUser);
         return token;
     } catch (error) {
